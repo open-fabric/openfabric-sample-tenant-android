d10108ad4614d87420455d76f9b129c52594f18d
Tung Dao <me@tungdao.com>
2023-08-22 11:14:02 +0700

    fix: SDK compatibility

--- app/src/main/kotlin/co/openfabric/tenant/sample/activity/WebViewActivity.kt
+++ app/src/main/kotlin/co/openfabric/tenant/sample/activity/WebViewActivity.kt
@@ -42,8 +42,9 @@ class WebViewActivity : AppCompatActivity(), NavigationListener, ErrorListener {
 
         sdk = UnilateralSDK.initialize(
             TenantConfiguration(
+                "Home Credit Qwarta",
+                URL("https://chatbot.homecredit.ph/assets/visual/icons/smile-logo_outline.svg"),
                 "Home Credit",
-                URL("https://homecredit.vn/img/logo-hc-main.png"),
             ),
             PartnerConfiguration(
                 merchant!!.accessToken,
@@ -52,6 +53,7 @@ class WebViewActivity : AppCompatActivity(), NavigationListener, ErrorListener {
             webView,
             Environment.DEV
         )
+//        sdk.setDebug(true)
         sdk.setNavigationListener(this)
 
         webView.loadUrl(merchant.url.toString())
