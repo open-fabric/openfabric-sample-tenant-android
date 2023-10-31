7de2f611b70bb21dd638e65cc45ac3468ec57031
Tung Dao <me@tungdao.com>
2023-10-31 15:09:09 +0700

    fix: conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,11 +13,7 @@ android {
         minSdk = 23
         targetSdk = 33
         versionCode = 1
-<<<<<<< Updated upstream
-        versionName = "1.1"
-=======
         versionName = "1.4"
->>>>>>> Stashed changes
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
